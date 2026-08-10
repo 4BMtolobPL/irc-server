d21b5a75416add283defeabd7647c3a697bbf8c1
4BMtolobPL <108853516+4BMtolobPL@users.noreply.github.com>
2026-08-10 18:06:25 +0900

    feat: implement client management and messaging
    Introduce Server and Client structs to track connected users and use
    MPSC channels to handle asynchronous bidirectional communication.

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,112 @@
+use std::{collections::HashMap, sync::Arc};
+
 use tokio::{
-    io::{AsyncBufReadExt, BufReader},
+    io::{AsyncBufReadExt, AsyncWriteExt, BufReader},
     net::{TcpListener, TcpStream},
+    sync::{RwLock, mpsc},
 };
 
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let addr = "0.0.0.0:6667";
-    let listener = TcpListener::bind(addr).await?;
+type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
+type ClientId = u64;
 
-    println!("IRC server listening on {addr}");
+#[derive(Debug, Default)]
+struct Server {
+    clients: HashMap<ClientId, Client>,
+}
+
+#[derive(Debug)]
+struct Client {
+    nickname: Option<String>,
+    username: Option<String>,
+    sender: mpsc::Sender<String>,
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let listen_addr = "0.0.0.0:6667";
+    let listener = TcpListener::bind(listen_addr).await?;
+
+    let server = Arc::new(RwLock::new(Server::default()));
+    let mut next_client_id: ClientId = 1;
+
+    println!("IRC server listening on {listen_addr}");
 
     loop {
         let (stream, addr) = listener.accept().await?;
 
-        println!("Client connected: {addr}");
+        let client_id = next_client_id;
+        next_client_id += 1;
+
+        println!("Client {client_id} connected: {addr}");
+
+        let server = Arc::clone(&server);
 
         tokio::spawn(async move {
-            if let Err(err) = handle_client(stream).await {
-                eprintln!("Client error: {err}");
+            if let Err(err) = handle_client(stream, client_id, server).await {
+                eprintln!("Client {client_id} error: {err}");
             }
         });
     }
 }
 
-async fn handle_client(stream: TcpStream) -> Result<(), Box<dyn std::error::Error>> {
-    let reader = BufReader::new(stream);
-    let mut lines = reader.lines();
+async fn handle_client(
+    stream: TcpStream,
+    client_id: ClientId,
+    server: Arc<RwLock<Server>>,
+) -> Result<()> {
+    let (reader, mut writer) = stream.into_split();
 
-    while let Some(line) = lines.next_line().await? {
-        println!("Received: {line}");
+    let (sender, mut receiver) = mpsc::channel::<String>(100);
+
+    {
+        let mut server = server.write().await;
+
+        server.clients.insert(
+            client_id,
+            Client {
+                nickname: None,
+                username: None,
+                sender: sender.clone(),
+            },
+        );
     }
+
+    sender.send("Welcome to my IRC server!".to_string()).await?;
+
+    let read_task = async {
+        let reader = BufReader::new(reader);
+        let mut lines = reader.lines();
+
+        while let Some(line) = lines.next_line().await? {
+            println!("[{client_id}] {line}");
+        }
+
+        Ok::<(), Box<dyn std::error::Error>>(())
+    };
+
+    let write_task = async {
+        while let Some(message) = receiver.recv().await {
+            writer.write_all(message.as_bytes()).await?;
+            writer.write_all(b"\r\n").await?;
+        }
+
+        Ok::<(), Box<dyn std::error::Error>>(())
+    };
+
+    tokio::select! {
+        result = read_task => {
+            result?;
+        }
+        result = write_task => {
+            result?;
+        }
+    }
+
+    {
+        let mut server = server.write().await;
+        server.clients.remove(&client_id);
+    }
+    println!("Client {client_id} disconnected");
 
     Ok(())
 }
